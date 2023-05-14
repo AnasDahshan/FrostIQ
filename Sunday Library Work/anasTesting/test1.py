6ce34f2216c8ada07f1312b8173705fb3ed40312
Anas Dahshan <107579439+AnasDahshan@users.noreply.github.com>
2023-05-14 20:03:14 +0300

    Update test1.py

--- Sunday Library Work/anasTesting/test1.py
+++ Sunday Library Work/anasTesting/test1.py
@@ -1,7 +1,7 @@
 import cv2
 from pyzbar import pyzbar
 import RPi.GPIO as GPIO
-import time
+import json
 
 class Button:
     def __init__(self, pin):
@@ -16,15 +16,22 @@ class Button:
 
 def read_barcodes(frame):
     barcodes = pyzbar.decode(frame)
+    barcode_list = []
     for barcode in barcodes:
         x, y, w, h = barcode.rect
         barcode_info = barcode.data.decode('utf-8')
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
-        with open("barcode_result.txt", mode='w') as file:
-            file.write("Recognized Barcode: " + barcode_info)
-    return frame
+        barcode_list.append(barcode_info)
+    
+    return frame, barcode_list
+
+def save_barcodes(barcode_list):
+    with open("ScannedBarcodes.json", "a") as file:
+        for barcode in barcode_list:
+            json.dump({"Barcode": barcode}, file)
+            file.write("\n")
 
 def main():
     buttonOn = Button(23)  # GPIO pin 23 for the camera on button
@@ -34,22 +41,30 @@ def main():
     is_camera_on = False  # Flag to keep track of camera state
     prev_button_on_state = False
     prev_button_off_state = False
+    scanned_barcodes = []  # List to store scanned barcode values
 
     while ret:
         if not prev_button_on_state and buttonOn.is_pressed():
             is_camera_on = not is_camera_on  # Toggle camera state
-            print("Camera is", "on" if is_camera_on else "off")
+            if is_camera_on:
+                print("Camera is on")
+            else:
+                print("Camera is off")
 
         if not prev_button_off_state and buttonOff.is_pressed():
             is_camera_on = False
             print("Camera is off")
+            if scanned_barcodes:
+                save_barcodes(scanned_barcodes)
+                scanned_barcodes = []
 
         prev_button_on_state = buttonOn.is_pressed()
         prev_button_off_state = buttonOff.is_pressed()
 
         if is_camera_on:
             ret, frame = camera.read()
-            frame = read_barcodes(frame)
+            frame, barcode_list = read_barcodes(frame)
+            scanned_barcodes.extend(barcode_list)
             cv2.imshow('Real Time Barcode Scanner', frame)
 
         if cv2.waitKey(1) & 0xFF == 27:
