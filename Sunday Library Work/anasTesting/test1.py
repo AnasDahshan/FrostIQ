a320a2aa29e3135467b3dd57ccc494d07e4e62ed
Anas Dahshan <107579439+AnasDahshan@users.noreply.github.com>
2023-05-14 19:54:30 +0300

    Update test1.py

--- Sunday Library Work/anasTesting/test1.py
+++ Sunday Library Work/anasTesting/test1.py
@@ -11,47 +11,41 @@ class Button:
 
     def is_pressed(self):
         if GPIO.input(self.pin) == GPIO.HIGH:
-            print("Button pressed!")
             return True
-        return False
-
-    def is_double_pressed(self):
-        if self.is_pressed():
-            time.sleep(0.1)
-            if self.is_pressed():
-                return True
         return False
 
 def read_barcodes(frame):
     barcodes = pyzbar.decode(frame)
     for barcode in barcodes:
         x, y, w, h = barcode.rect
-        # 1
         barcode_info = barcode.data.decode('utf-8')
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # 2
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
-        # 3
         with open("barcode_result.txt", mode='w') as file:
             file.write("Recognized Barcode: " + barcode_info)
-
     return frame
 
 def main():
-    button = Button(23)  # GPIO pin 23 for the button
+    buttonOn = Button(23)  # GPIO pin 23 for the camera on button
+    buttonOff = Button(24)  # GPIO pin 24 for the camera off button
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
     is_camera_on = False  # Flag to keep track of camera state
+    prev_button_on_state = False
+    prev_button_off_state = False
 
     while ret:
-        if button.is_double_pressed():
+        if not prev_button_on_state and buttonOn.is_pressed():
             is_camera_on = not is_camera_on  # Toggle camera state
-            if is_camera_on:
-                print("Camera is on")
-            else:
-                print("Camera is off")
+            print("Camera is", "on" if is_camera_on else "off")
+
+        if not prev_button_off_state and buttonOff.is_pressed():
+            is_camera_on = False
+            print("Camera is off")
+
+        prev_button_on_state = buttonOn.is_pressed()
+        prev_button_off_state = buttonOff.is_pressed()
 
         if is_camera_on:
             ret, frame = camera.read()
